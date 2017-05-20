9d72769571b5543b1779e3e5d44aef9944d66802
Julio Augusto Vera Sancho <Julio Augusto Vera Sancho>
2017-05-20 11:32:52 -0500

    update models

--- Camping/Models/Services/DataAccess.cs
+++ Camping/Models/Services/DataAccess.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using SQLite.Net;
 using Xamarin.Forms;
 using System.IO;
-
+using MLearningDB;
 
 namespace Camping.Models.Services
 {
@@ -19,13 +19,16 @@ namespace Camping.Models.Services
             connection = new SQLiteConnection(config.Plataforma, Path.Combine(config.DirectorioDB, "cache.db"));
         }
 
-        
 
-        
+
+        public List<circle_by_user> GetCirclesByUser(int userid) {
+            //System.Linq.Expressions.Expression<Func<circle_by_user, bool>> predicate = c => c.User_id == userid ;
+            return connection.Table<circle_by_user>().Where(c => c.User_id == userid).ToList<circle_by_user>();
+        }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            connection.Dispose();
         }
     }
 }
